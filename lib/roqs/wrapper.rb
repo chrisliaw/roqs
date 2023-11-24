7a77e2ac58112ebf9fe04b1bff9094a452ed2968
Chris <chris@antrapol.com>
2023-11-24 23:42:45 +0800

    Adding release-gem and support dynamic loading of liboqs

--- lib/roqs/wrapper.rb
+++ lib/roqs/wrapper.rb
@@ -25,18 +25,41 @@ module Roqs
         end
       end
 
-      def load_arch_lib(os)
+      def load_arch_lib(os) 
         plat = RUBY_PLATFORM
         pplat = plat.split('-')[0]
         logger.debug "OS architecture is #{pplat}"
-        drvDir = File.join(File.dirname(__FILE__),"..","..","native","#{os}",pplat)
+        usrDrvDir = ENV['ROQS_LIBOQS_DIR']
+        if not_empty?(usrDrvDir)
+          logger.debug "Load liboqs shared library from user provided root : #{File.join(usrDrvDir, pplat)}" 
+          drvDir = File.join(usrDrvDir,pplat)
+        else
+          logger.debug "Load liboqs shared library from Roqs internal root : #{File.join(File.dirname(__FILE__),"..","..","native","#{os}",pplat)}" 
+          drvDir = File.join(File.dirname(__FILE__),"..","..","native","#{os}",pplat)
+        end
+
         if File.exist?(drvDir)
           Dir.glob(File.join(drvDir,"liboqs*")) do |f|
-            logger.debug "Loading #{f} from system"
+            logger.debug "Loading liboqs at : #{f}"
             dlload f
           end
         else
-          raise WrapperError, "Directory '#{pplat}' not found at #{drvDir}"
+          errMsg = []
+          errMsg << "Shared library of liboqs (https://github.com/open-quantum-safe/liboqs) could not be found at '#{drvDir}'"
+          errMsg << "You might need to compile for your platform."
+          errMsg << "Simply follow the steps beflow:"
+          errMsg << "1. Clone the repository at https://github.com/open-quantum-safe/liboqs"
+          errMsg << "   > clone https://github.com/open-quantum-safe/liboqs liboqs"
+          errMsg << "2. Make sure all pre-requisites are installed as documented here : https://github.com/open-quantum-safe/liboqs#quickstart"
+          errMsg << "3. Run the following commands: "
+          errMsg << "   > cd liboqs && mkdir build && cd build"
+          errMsg << "   > cmake -GNinja .. -DBUILD_SHARED_LIBS=ON"
+          errMsg << "   > ninja"
+          errMsg << "4. Shared library shall be built under lib dir."
+          errMsg << "5. Copy the shared library into '#{drvDir}' and you should be good to go"
+          errMsg << "   Note: System shall read env variable 'ROQS_LIBOQS_DIR' to decide where to load the liboqs dynamic library from."
+          errMsg << "         If no value is given, system shall load from default path : #{File.join(File.dirname(__FILE__),"..","..","native","#{os}",pplat)}"
+          raise WrapperError, errMsg.join("\n")
         end
       end
 
